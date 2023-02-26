fcc2ba3b35bcd79bce7fb7987622b14fa80930e6
Muktansh Chaturvedi <muktansh@gmail.com>
2023-02-26 11:36:00 +0000

    camera reader opencv

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,19 @@
 #include<iostream>
 #include "simplecameralib/printhello.h"
+#include "simplecameralib/Camera_reader.h"
 
 int main() {
 	printhello();
+
+	Mat frame;
+	string window_name = "Captured Image";
+
+	CameraReader C;
+	frame = C.FeedCapture();
+	namedWindow(window_name);
+
+	imshow(window_name, frame);
+	waitKey(0);
+
 	return 0;
 }
