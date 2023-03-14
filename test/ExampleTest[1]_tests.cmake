e0b008c976cd5b3a0d390a923dc8c9dddeae1c63
muktansh <muktansh@gmail.com>
2023-03-14 19:20:08 +0000

    Delete ExampleTest[1]_tests.cmake

--- test/ExampleTest[1]_tests.cmake
+++
@@ -1,3 +0,0 @@
-add_test( ExampleTests.DemonstrateGTestMacros /mnt/d/matapos_6_March/matapos/test/ExampleTest [==[--gtest_filter=ExampleTests.DemonstrateGTestMacros]==] --gtest_also_run_disabled_tests)
-set_tests_properties( ExampleTests.DemonstrateGTestMacros PROPERTIES WORKING_DIRECTORY /mnt/d/matapos_6_March/matapos/test SKIP_REGULAR_EXPRESSION [==[\[  SKIPPED \]]==])
-set( ExampleTest_TESTS ExampleTests.DemonstrateGTestMacros)
