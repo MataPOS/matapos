4c6d25988ebe01af5ace061cfb88342c1c3ea5eb
fatherofcamels <ayusuf13@hotmail.co.uk>
2023-03-22 18:50:18 +0000

    First iteration header

--- src/numpad.h
+++ src/numpad.h
@@ -1,20 +1,39 @@
 #ifndef NUMPAD
 #define NUMPAD
 
+#include <iostream>
+#include <thread>
+#include <vector>
+
+
+struct NumpadCallback{
+    public virtual void hasData(unsigned long number) = 0;
+};
+
 
 class NumpadDriver{
 
 
     private:
 
+    std::thread t;
+    int wake(int);
+    void Readnumpad();
+    std::vector<NumpadCallback*>numpadcallback;
+    std::vector<int>data;
+
+    vector<int> insertAtEnd(vector<int> v, int x);
 
     public:
 
         NumpadDriver();
 
+        void registerCallback(NumpadCallback* lc);
+
         void start();
 
-        void read();
+        void readNumpad();
+
 
         
 
