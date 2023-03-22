30b19d4f9578a96b02b9a0cb633376e7d7be9132
fatherofcamels <ayusuf13@hotmail.co.uk>
2023-03-22 18:50:03 +0000

    first iteration of numpad Driver

---
+++ src/numpad.cpp
@@ -0,0 +1,136 @@
+# include <numpad.h>
+#include <stdio.h>
+#include <vector.h>
+
+
+#include <iostream>
+#include <thread>
+#include <pigpio.h>
+
+int NumpadDriver::wake(int data_pin)
+{
+    t = thread::(&NumpadDriver::Readnumpad, this);
+}
+
+void NumpadDriver::Readnumpad()
+{
+
+}
+
+void NumpadDriver::registerCallback(NumpadCallback *lc)
+{
+}
+
+vector<int> NumpadDriver::insertAtEnd(vector<int> v, int x){
+    v.push_back(x);
+    return v;
+}
+
+void NumpadDriver::readNumpad()
+{
+    int data = [0,0,0,0];
+
+    int r1 = 7 ,r2 = 9 ,r3 = 11, r4 = 13 ,c1 = 15,c2 = 17 ,c3 = 19,c4 = 21;
+
+    //Initialising GPIO Pins
+    if (gpioInitialise() < 0)
+    {
+        fprintf(stderr, "pigpio initialisation failed\n");
+        return 1;
+    }
+
+    //Setting GPIO Pins as Input
+    gpioSetMode(r1, PI_INPUT);
+    gpioSetMode(r2, PI_INPUT);
+    gpioSetMode(r3, PI_INPUT);
+    gpioSetMode(r4, PI_INPUT);
+    gpioSetMode(c1, PI_INPUT);
+    gpioSetMode(c2, PI_INPUT);
+    gpioSetMode(c3, PI_INPUT);
+    gpioSetMode(c4, PI_INPUT);
+
+    //Setting Pull Up Resistor
+    gpioSetPullUpDown(r1, PI_PUD_UP);
+    gpioSetPullUpDown(r2, PI_PUD_UP);
+    gpioSetPullUpDown(r3, PI_PUD_UP);
+    gpioSetPullUpDown(r4, PI_PUD_UP);
+    gpioSetPullUpDown(c1, PI_PUD_UP);
+    gpioSetPullUpDown(c2, PI_PUD_UP);
+    gpioSetPullUpDown(c3, PI_PUD_UP);
+    gpioSetPullUpDown(c4, PI_PUD_UP);
+
+    int lookup[4][3] = {{1,2,3},{4,5,6},{7,8,9},{10,0,11}};
+
+while (1)
+    if (gpioRead(r1) == 0)
+    {
+        if (gpioRead(c1) == 0)
+        {
+            data = insertAtEnd(data,lookup[0][0]);
+        }
+        else if (gpioRead(c2) == 0)
+        {
+            data = insertAtEnd(data,lookup[0][0]);
+        }
+        else if (gpioRead(c3) == 0)
+        {
+            data = insertAtEnd(data,lookup[0][0]);
+        }
+    }
+    else if (gpioRead(r2) == 0)
+    {
+        if (gpioRead(c1) == 0)
+        {
+            data = insertAtEnd(data,lookup[0][0]);
+        }
+        else if (gpioRead(c2) == 0)
+        {
+            data = insertAtEnd(data,lookup[0][0]);
+        }
+        else if (gpioRead(c3) == 0)
+        {
+            data = insertAtEnd(data,lookup[0][0]);
+        }
+    }
+    else if (gpioRead(r3) == 0)
+    {
+        if (gpioRead(c1) == 0)
+        {
+            data = insertAtEnd(data,lookup[0][0]);
+        }
+        else if (gpioRead(c2) == 0)
+        {
+            data = insertAtEnd(data,lookup[0][0]);
+        }
+        else if (gpioRead(c3) == 0)
+        {
+            data = insertAtEnd(data,lookup[0][0]);
+        }
+    }
+    else if (gpioRead(r4) == 0)
+    {
+        if (gpioRead(c1) == 0)
+        {
+            data = insertAtEnd(data,lookup[0][0]);
+        }
+        else if (gpioRead(c2) == 0)
+        {
+            data = insertAtEnd(data,lookup[0][0]);
+        }
+        else if (gpioRead(c3) == 0)
+        {
+            data = insertAtEnd(data,lookup[0][0]);
+        }
+    }
+    else
+    {
+        data[0] = 0;
+    }
+
+
+
+
+
+}
+
+
