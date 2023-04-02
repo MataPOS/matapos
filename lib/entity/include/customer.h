a4698cc78b112cd29b6c10ea98a6ea72cc77987f
avinashkrishnan2020 <avinashkrishnan2020@gmail.com>
2023-04-02 23:25:10 +0100

    created stock table and created classes for customer and stock

---
+++ lib/entity/include/customer.h
@@ -0,0 +1,14 @@
+#include <iostream>
+
+class Customer {
+
+	public:
+		int id;
+		std::string uniqueId;
+		std::string firstName;
+		std::string lastName;
+		std::cardNumber;
+		std::emailId;
+	
+
+};
---
+++ lib/entity/include/stock.h
@@ -0,0 +1,12 @@
+#include <iostream>
+
+class Stock {
+
+public:
+	int id;
+	std::string uniqueId;
+	std::string itemName;
+	float price;
+	int qty;
+
+};
