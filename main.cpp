7a7be0465fc302aa4b80efd7b56b90a06c0d0997
muktansh <muktansh@gmail.com>
2023-04-11 19:11:01 +0100

    Updated - Camera, gui, and window
    Camera - created singleton camera class for multiple thread access to single camera driver.
    GUI - created Welcome Screen, & Cart Screen. Implemented callbacks for the push buttons.
    Window - main program controller, which will implement all program logic, and state transitions.

---
+++ main.cpp
@@ -0,0 +1,19 @@
+
+#include "window.h"
+#include<QApplication>
+
+
+int main(int argc, char *argv[])
+{
+	QApplication app(argc,argv);
+	
+	window *w;
+	w = new window();
+
+	w->start();
+	
+	
+	
+	return app.exec();
+
+}
