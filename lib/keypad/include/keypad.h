1b5e5bddd3978024a12204cd88c9175bd8e2b6fc
fatherofcamels <ayusuf13@hotmail.co.uk>
2023-04-17 17:18:30 +0100

    Attempt with pointers

--- lib/keypad/include/keypad.h
+++ lib/keypad/include/keypad.h
@@ -8,9 +8,14 @@
 
 struct NumpadCallback{
     public : virtual void nums(std::vector<long unsigned int>) = 0;
-    public : virtual void backwards(int) = 0;
-    public : virtual void forwards(int) = 0;
 
+};
+struct forwardCallback{
+    public : virtual void forwards(int) = 0;
+};
+
+struct backCallback{
+    public : virtual void backwards(int) = 0;
 };
 
 
@@ -22,27 +27,62 @@ class NumpadDriver{
     void readNumpad();
 
     void stop();
-    
+    backCallback* back;
+    forwardCallback* forward;
         
     //register Callbacks
     void KeypadCallbacks(NumpadCallback* np);
 
     //Callback functions
-    void backwards(int);
+    void forwards(forwardCallback* fwd);
+    void backwards(backCallback* bck);
 
-    //Interupt handler
-    void interuptHandler(int gpio, int level, uint32_t tick);
+    ~NumpadDriver();
 
-
- private:
-    int back; //flag for back button
-    int forward;//flag for forward button
+    //int back; //flag for back button
+    //int forward;//flag for forward button
 
     std::thread keypadthread;
 
     std::vector<NumpadCallback*> callbacks; // Storing the numpad callbacks
 
-    Numpad(); //constructor
+    NumpadDriver(); //constructor
+
+    //Interupt handler
+    void static interuptHandler(int gpio, int level, uint32_t tick)
+{
+    static int state_1 = -1;//Forwards
+    static int state_2 = -1;//Forwards
+    static int state_3 = -1;//Backwards
+
+    if (gpio == 13)
+    {
+        state_1 = level;
+
+    } else if (gpio == 15)
+    {
+        state_2 = level;
+    }else if (gpio == 19)
+    {
+        state_3 = level;}
+
+    if (state_2 == 0 && state_1 == 0)
+    {
+        int back = 1;
+        for (auto  back: backCallback)
+        {
+            back->backwards(0);
+        }
+    }
+    else if (state_1 == 0 && state_3 == 0)
+    {
+        int forward = 1;
+        for (auto  forward: forwardCallback)
+        {
+            forward->forwards(0);
+        }
+    }
+}
 };
 
 
--- lib/keypad/src/keypad.cpp
+++ lib/keypad/src/keypad.cpp
@@ -9,16 +9,48 @@
 #include<thread>
 #include<pigpio.h>
 
-int NumpadDriver::wake(int data_pin)
+void NumpadDriver::wake(int data_pin)
 {
     std::thread keypadthread(&NumpadDriver::readNumpad, this);
 
-    t.join();
-    return 1;
+    keypadthread.join();
+    //return 1;
 }
+
+// Callback registrations
+
+void NumpadDriver::KeypadCallbacks(NumpadCallback* np)
+{
+    NumpadCallback.push_back(np);
+}
+//Add function to go back and forward.
+
+//Callback functions
+
+
+void NumpadDriver::forwards(forwardCallback* bck)
+{
+    forwardCallback.push_back(np);
+      #ifdef DEBUG
+		std::cout << std::endl << "forward button has been pressed" << std::endl;
+	#endif
+    //forward = 1; 
+
+}
+
+void backwards(backCallback* fwd)
+{
+    backCallback.push_back(np);
+    #ifdef DEBUG    
+		std::cout << std::endl << "back button has been pressed" << std::endl;
+	#endif
+
+}
+
 void NumpadDriver::readNumpad()
 {
-    std::vector<long unsigned int> data;
+    //std::vector<long unsigned int> data;
+    *data dat
 
     long unsigned int lookup[4][3] = {{1,2,3},{4,5,6},{7,8,9},{10,0,11}};
     int r1 = 7 ,r2 = 9 ,r3 = 11, r4 = 13 ,c1 = 15,c2 = 17 ,c3 = 19;
@@ -117,71 +149,15 @@ void NumpadDriver::readNumpad()
             data[0] = 0;
         }
     }
-    for (auto np : NumpadCallback)
+    for (auto nums : NumpadCallback)
     {
-        np->nums(data);
+        dat->nums(data);
     }
 
 }
 
-//Interrupt handler
-void void NumpadDriver::interuptHandler(int gpio, int level, uint32_t tick)
-{
-    static int state_1 = -1;//Forwards
-    static int state_2 = -1;//Forwards
-    static int state_3 = -1;//Backwards
 
-    if (gpio == 13)
-    {
-        state_1 = level;
 
-    } else if (gpio == 15)
-    {
-        state_2 = level;
-    }else if (gpio == 19)
-    {
-        state_3 = level;
-
-    if (state_2 == 0 && state_1 == 0)
-    {
-        for (auto np : NumpadCallback)
-        {
-            np->backwards(0);
-        }
-    }
-    else if (state_1 == 0 && state_3 == 0)
-    {
-        for (auto np : NumpadCallback)
-        {
-            np->forwards(0);
-        }
-    }
-}
-
-// Callback registrations
-
-void NumpadDriver::KeypadCallbacks(NumpadCallback* np)
-{
-    numpadcallback.push_back(np);
-}
-//Add function to go back and forward.
-
-//Callback functions
-void NumpadDriver::backwards(int data)
-{
-    #ifdef DEBUG
-		std::cout << std::endl << "Back button has been pressed" << std::endl;
-	#endif
-
-}
-
-void NumpadDriver::forwards(int data)
-{
-    #ifdef DEBUG
-		std::cout << std::endl << "Back button has been pressed" << std::endl;
-	#endif
-    //forward = 1;
-}
 
 void stop()
 {
